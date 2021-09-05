3d9fb563bb7e78791f9b0f818918b9ef0169a942
Gabriel <gdlmartins@gmail.com>
2021-09-05 19:55:24 +0800

    new components and new expense and cancel botton

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expense.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expense = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,17 +12,14 @@ const Expense = (props) => {
     setFilteredYear(onYearhandler);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onYearhandler={Yearhandler} />
-      {console.log(props.expense)}
-      {props.expense.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
